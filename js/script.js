b0227acc231a2d419bf6a6e773d738c364acb522
Pierre AS Speakman <97603002+speakman90@users.noreply.github.com>
2022-05-03 15:58:45 +0200

    V0.3
    Add sprite + modify background

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 const imgIdle = 'style/img/player1/Martial Hero/Sprites/Idle.png'
+const imgJump = 'style/img/player1/Martial Hero/Sprites/Jump.png'
+const imgFall = 'style/img/player1/Martial Hero/Sprites/Fall.png'
+const imgRun = 'style/img/player1/Martial Hero/Sprites/Run.png'
+
 
 
 const canvas = document.querySelector('canvas');
@@ -7,13 +11,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-function createImage(imageSrc){
-    const image = new Image()
-    image.src = imageSrc
-    return image
-}
-
-const gravity = 0.4
+const gravity = 5
 
 var Keys = {
     up: false,
@@ -32,34 +30,42 @@ class Player {
             x: 0,
             y: 5
         }
-        this.width = 300
-        this.height = 600 
-        this.image = createImage(imgIdle)
-        this.frames = 1
+        ctx.fillStyle = "#FF0000";
+        this.width = 200
+        this.height = 400
+        this.frames = 0
+        this.sprites = {
+            stand: {
+                jump: createImage(imgJump),
+                fall: createImage(imgFall),
+                cropWidth: 200
+            },
+            run: {
+                right: createImage(imgRun),
+                cropWidth: 200
+            },
+            idle: {
+                idle: createImage(imgIdle),
+                cropWidth: 200
+            }
+        }
+        this.currentSprite = this.sprites.idle.idle
+        this.currentCropWidth = 200
     }
 
     draw() {
-        ctx.drawImage(
-        this.image,
-        0,
-        0,
-        150,
-        300,
-        this.position.x, 
-        this.position.y, this.width, this.height)
+        this.frames++
+        if(this.frames > 1.5) this.frames = 0
+        ctx.drawImage(this.currentSprite,this.currentCropWidth * this.frames,0,this.currentCropWidth,500,this.position.x,this.position.y, this.width, this.height)
         if (this.position.y + this.velocity.y > canvas.height || this.position.y + this.velocity.y < 0) {
             this.velocity.y = -this.velocity.y;
         }
-        if (this.position.x + this.velocity.x > (canvas.width-this.witdh) || this.position.x + this.velocity.x < 0) {
+        if (this.position.x + this.velocity.x > 1720 || this.position.x + this.velocity.x < -200) {
             this.velocity.x = -this.velocity.x;
         }
     }
 
     update() {
-        this.frames++
-        if (this.frames > 10) {
-            this.frames = 0
-        }
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
@@ -68,8 +74,7 @@ class Player {
         } else {
             this.velocity.y = 0
         }
-        if(this.position.x > canvas.width) {
-            console.log(this.position.x)
+        if(this.position.x > (canvas.width - this.width)) {
             this.velocity.x = 0
         }
     }
@@ -81,30 +86,61 @@ const player = new Player();
 
 function animate(){
     requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    player.update()
+    now = Date.now();
+    elapsed = now - then;
 
-    if (Keys.right === true) {
-        player.velocity.x = 5
+    // if enough time has elapsed, draw the next frame
+
+    if (elapsed > fpsInterval) {
+
+        // Get ready for next frame by setting then=now, but also adjust for your
+        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+        then = now - (elapsed % fpsInterval);
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        player.update()
+        if (Keys.right === true) {
+            player.velocity.x = 20
+        }
+        if (Keys.left === true) {
+            player.velocity.x = -20
+        }
     }
-    if (Keys.left === true) {
-        player.velocity.x = -5
-    }
+
 };
 
-animate()
+var stop = false;
+var frameCount = 0;
+var fps, fpsInterval, startTime, now, then, elapsed;
+
+function startAnimating(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    animate();
+}
+
+startAnimating(60)
+
+function createImage(imageSrc){
+    const image = new Image()
+    image.src = imageSrc
+    return image
+}
 
 window.addEventListener('keydown', (event) => {
     const nomTouche = event.key;
 
     if (' ' === nomTouche) {
-            player.velocity.y -= 10;
+            player.velocity.y -= 30;
+            player.currentSprite = player.sprites.stand.jump
         }
     if ('ArrowDown' === nomTouche) {
         Keys.down = true;
     }
     if ('ArrowRight' === nomTouche) {
         Keys.right = true;
+        player.currentSprite = player.sprites.run.right
+        player.currentCropWidth = player.sprite.run.cropWidth
         player.velocity.x += 1;
     }
     if ('ArrowLeft' === nomTouche) {
@@ -118,6 +154,8 @@ window.addEventListener('keyup', (event) => {
 
     if (' ' === nomTouche) {
         Keys.up = false;
+        player.currentSprite = player.sprites.stand.fall
+        player.currentCropWidth = player.sprite.stand.cropWidth
     }
     if ('ArrowDown' === nomTouche) {
         Keys.down = false;
